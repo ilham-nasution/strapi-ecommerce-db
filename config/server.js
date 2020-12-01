9e772b195dbadde0121d7c075021f6f6300ab919
Ilham Rahman <ilhaamrahman@gmail.com>
2020-12-01 14:49:48 +0700

    create backend for next js e-commerce app

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '79acf84b31007aa45dc2f3dd8969273e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '045874be-6ce6-42cf-8982-4f6a35b9f70e'
+};
